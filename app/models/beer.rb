756f096aca68eaa82023e6867623187befbc16b1
ismoaulaskari <ismo@aulaskari.com>
2014-01-26 20:40:38 +0200

    tehtävä 2_4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,6 +2,17 @@ class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
 
+  def average_rating
+    @sum = 0
+    @no = 0
+    self.ratings.each do |rate| 
+      @sum += rate.score
+      @no = @no + 1
+    end
+    @average = @sum / @no
+    "Has #{@no} ratings, average #{@average}"
+  end
+
   def to_s
     self.name
   end
