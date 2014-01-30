b6cd87a655efc67fad1a479ef233c8d7143297df
ismoaulaskari <ismo@aulaskari.com>
2014-01-30 19:42:40 +0200

    2_14-15 puuttuvat

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,30 +1,31 @@
 include ActionView::Helpers::TextHelper
 
 class Beer < ActiveRecord::Base
+  include RatingAverage
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
 
-  def average_rating
-    @sum = 0
-    @no = 0
-    self.ratings.each do |rate| 
-      @sum += rate.score
-      @no = @no + 1
-    end
-    if @no > 0
-      @average = @sum / @no
-    else
-      puts 0
-    end
+#  def average_rating2
+#    @sum = 0
+#    @no = 0
+#    self.ratings.each do |rate| 
+#      @sum += rate.score
+#      @no = @no + 1
+#    end
+#    if @no > 0
+#      @average = @sum / @no
+#    else
+#      puts 0
+#    end
    
 #    self.ratings.each.inject { |sum, n| sum + n }
 
     #@sum = (self.ratings.each).sum
 #    @average = @sum / self.ratings.count
 
-    @rates = pluralize(@no, "rating")
-    "Has #{@rates}, average #{@average}"
-  end
+#    @rates = pluralize(@no, "rating")
+#    "Has #{@rates}, average #{@average}"
+#  end
 
   def to_s
 #    "#{name} #{brewery.name}"
