a8513dae2b259a1fea63d73659e372b9f9c0eb3c
ismoaulaskari <ismo@aulaskari.com>
2014-01-31 22:40:33 +0200

    salasanan ennen tehtävää 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,17 +4,16 @@ class SessionsController < ApplicationController
     end
 
     def create
-      # haetaan usernamea vastaava käyttäjä tietokannasta
       user = User.find_by username: params[:username]
-      # talletetaan sessioon kirjautuneen käyttäjän id (jos käyttäjä on olemassa)
       session[:user_id] = user.id if not user.nil?
-      # uudelleen ohjataan käyttäjä omalle sivulleen 
-      if user.nil? 
-        redirect_to :back, notice: "User #{params[:username]} does not exist!"
+
+      if user.nil? or not user.authenticate params[:password]
+        redirect_to :back, notice: "username and password do not match"
       else
-        session[:user_id] = user.id 
-        redirect_to user, notice: "Welcome back!"
+        session[:user_id] = user.id
+        redirect_to user_path(user), notice: "Welcome back!"
       end
+
     end
 
     def destroy
