ccc4f271fe098c4aaa9a1bea02e1605d0192e412
ismoaulaskari <ismo@aulaskari.com>
2014-01-31 22:08:27 +0200

    tehtävän 6 ja 7 välistä

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   include RatingAverage
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
   has_many :ratings
+  has_many :beers, through: :ratings
 end
