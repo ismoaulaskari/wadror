89378108259a0f9d874017aa39ad8f2c2497938d
ismoaulaskari <ismo@aulaskari.com>
2014-01-31 16:29:44 +0200

    3_1

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Ratebeer::Application.routes.draw do
   
   resources :ratings, :only => [:index, :new, :create, :destroy]
 
+  resources :sessions, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
