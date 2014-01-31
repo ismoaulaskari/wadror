eb300aeb5f981bac0f84c369fe410e45884c58ca
ismoaulaskari <ismo@aulaskari.com>
2014-01-31 22:57:25 +0200

    3_10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   include RatingAverage
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :password, length: { minimum: 4 }
+  validates_format_of :password, {with: /[A-Z]/, with: /[0-9]/}
   has_many :ratings
   has_many :beers, through: :ratings
 
