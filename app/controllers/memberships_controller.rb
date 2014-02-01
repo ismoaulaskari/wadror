947ab7d6e48eacdc05a29691d9a773e00f47b5e6
ismoaulaskari <ismo@aulaskari.com>
2014-02-01 19:29:20 +0200

    3_7-8

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -26,10 +26,8 @@ class MembershipsController < ApplicationController
   # POST /memberships
   # POST /memberships.json
   def create
-    @user = current_user
-    #@beer_club_id = params.require(:membership).permit(:beer_club_id)
-    #@membership = Membership.new(beer_club_id:@beer_club_id, user_id:@user.id)
     @membership = Membership.new(membership_params)
+    #@membershib.user = current_user
 
     respond_to do |format|
       if @membership.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
   has_many :memberships
-  has_many :beer_clubs, through: :memberships
+  has_many :beer_clubs, -> { uniq }, through: :memberships
 
   has_secure_password
 end
