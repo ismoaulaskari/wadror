dfbd06ff22136fc5fab8a66e21a420419c7893a5
ismoaulaskari <ismo@aulaskari.com>
2014-02-15 22:23:10 +0200

    tehtävä 5_8

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -34,7 +34,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
+        format.html { redirect_to @membership.beer_club, notice: "#{current_user.username} welcome to the club!" }
         format.json { render action: 'show', status: :created, location: @membership }
       else
         format.html { render action: 'new' }
