0b66fdeb72ac8531eb54bb009da800eae8ecb547
ismoaulaskari <ismo@aulaskari.com>
2014-02-23 22:24:41 +0200

    tehtävä 6-17 kontrollerin cache

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,11 +2,21 @@ class RatingsController < ApplicationController
   before_action :ensure_that_signed_in, except: [:index, :show]
 
   def index
-    @ratings = Rating.includes(:beer,:user).all
-    @recent_ratings = Rating.includes(:beer,:user).recent
-    @top_breweries = Brewery.top(3)
-    @top_beers = Beer.includes(:brewery).top(3)
-    @top_users = User.includes(:ratings).top(3)
+    Rails.cache.write("recent_ratings", Rating.includes(:beer,:user).recent) if cache_does_not_contain_data_or_it_is_too_old
+    @recent_ratings = Rails.cache.read("recent_ratings")
+
+    Rails.cache.write("brewery_top", Brewery.top(3)) if cache_does_not_contain_data_or_it_is_too_old
+    @top_breweries = Rails.cache.read("brewery_top")
+
+    Rails.cache.write("beer_top", Beer.includes(:brewery).top(3)) if cache_does_not_contain_data_or_it_is_too_old
+    @top_beers = Rails.cache.read("beer_top")
+
+    Rails.cache.write("user_top", User.includes(:ratings).top(3)) if cache_does_not_contain_data_or_it_is_too_old
+    @top_users = Rails.cache.read("user_top")
+
+    #tämä kirjoitettava viimeisenä tai kosahtaa!
+    Rails.cache.write("ratings-all", Rating.includes(:beer,:user).all, expires_in:2.minutes) if cache_does_not_contain_data_or_it_is_too_old
+    @ratings = Rails.cache.read("ratings-all")
   end
 
   def new
@@ -34,5 +44,10 @@ class RatingsController < ApplicationController
     rating = Rating.find(params[:id])
     rating.delete if current_user == rating.user
     redirect_to :back 
+  end
+
+  private
+  def cache_does_not_contain_data_or_it_is_too_old
+    Rails.cache.read("ratings-all").nil?
   end
 end
